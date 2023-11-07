b53f282952d2d7e369b19abc02b5dd398c3375d3
Alex <127699880+AlexanderMoraes7@users.noreply.github.com>
2023-11-07 17:25:00 -0300

    Create No.java

---
+++ No.java
@@ -0,0 +1,25 @@
+public class No {
+    private No proximo;
+    private Object elemento;
+    
+    public No(Object elemento, No proximo) {
+        this.elemento = elemento;
+        this.proximo = proximo;
+    }
+
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+    }
+
+    public No getProximo() {
+        return proximo;
+    }
+
+    public void SetElemento(Object elemento) {
+        this.elemento = elemento;
+    }
+
+    public Object getElemento() {
+        return elemento;
+    }
+}
